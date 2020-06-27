4eb6d5bed96b09120636b03c093bc2557594ce5e
Mark Ogilvie <mogilvie@users.noreply.github.com>
2020-06-27 18:40:30 +0100

    Add an example of customExport

---
+++ src/pages/docs/features/export/overriding-custom-export-function.js
@@ -0,0 +1,29 @@
+function OverridingCustomExportFunction() {
+  return (
+    <MaterialTable
+      title="Overriding Export Function Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+        {
+          title: 'Mother',
+          field: 'family',
+          customExport: rowData => rowData.family.mother
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63, family: {mother:'Maisy', father:'Malcolm'} },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34, family: {mother:'Zoe', father:'Zac'} },
+      ]}        
+      options={{
+        exportButton: true
+      }}
+    />
+  )
+}
